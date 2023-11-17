145f49203b32b5a9cfff865057e2ad4a29481a17
Venkatraman Renganathan <venkat@control.lth.se>
2023-11-17 16:15:54 +0100

    Add files via upload

--- Simulation.m
+++ Simulation.m
@@ -21,21 +21,42 @@ Parameters
 t = p.T0 : p.Ts : p.Tf;
 
 % System states
-x_numeric_exact = zeros(p.nx, length(t)); x_numeric_exact(:, 1) = [10; 0];
+x_numeric_exact = zeros(p.nx, length(t)); 
+x_numeric_exact(:, 1) = [10; 0];
 x_numeric_robust = x_numeric_exact;
+x_closed_loop_analytic_exact = x_numeric_exact;
+x_closed_loop_analytic_robust = x_numeric_exact;
 
 % Control input
 u_numeric_exact = zeros(p.nu, length(t)-1);
 u_numeric_robust = u_numeric_exact;
+u_closed_loop_analytic_exact = u_numeric_exact;
 
 u_analytic_exact = zeros(p.nu, length(t)-1);
 u_analytic_robust = u_analytic_exact;
+u_closed_loop_analytic_robust = u_numeric_robust;
+
+P_closed_loop_analytic_exact = zeros(length(t)-1, p.nx);
+P_closed_loop_analytic_robust = zeros(length(t)-1, p.nx);
+q_closed_loop_analytic_exact = zeros(1, length(t)-1);
+q_closed_loop_analytic_robust = zeros(1, length(t)-1);
+gamma_closed_loop_exact = zeros(1, length(t)-1);
+gamma_closed_loop_robust = zeros(1, length(t)-1);
+running_sum_exact = zeros(p.nx, length(t)-1);
+running_sum_robust = zeros(p.nx, length(t)-1);
+psi_exact = zeros(length(t)-1, p.nx);
+psi_robust = zeros(length(t)-1, p.nx);
+
+% Compute disturbance (the same for both cases for comparability)
+w = mvlaprnd(2, zeros(2,1), p.Sigma_w, length(t)-1); % w = mvnrnd([0 0], p.Sigma_w)';
 
 % Regret
-regret_total = zeros(1, length(t)-1);  
-regret_numeric = zeros(1, length(t)-1);
-regret_analytic = zeros(1, length(t)-1);
-regret_closed_loop = zeros(1, length(t)-1);
+regret_total_numeric = zeros(1, length(t)-1);  
+regret_total_analytic = zeros(1, length(t)-1);  
+regret_open_loop_numeric = zeros(1, length(t)-1);
+regret_open_loop_analytic = zeros(1, length(t)-1);
+regret_closed_loop_numeric = zeros(1, length(t)-1);
+regret_closed_loop_analytic = zeros(1, length(t)-1);
 
 
 %% === Closed-Loop Simulation ========================================== %%
@@ -68,25 +89,20 @@ for k = 1 : 1 : length(t)-1
     u_numeric_exact(k) = u_seq_numeric_exact(1);
     u_numeric_robust(k) = u_seq_numeric_robust(1);
     
-    % Compute disturbance (the same for both cases for comparability)
-    % w = mvnrnd([0 0], p.Sigma_w)';
-    w = mvlaprnd(2, zeros(2,1), p.Sigma_w, 1);
-    
-    
     % Apply optimal input to the system
-    x_numeric_exact(:, k+1) = p.model.A * x_numeric_exact(:, k) + p.model.B * u_numeric_exact(k) + w;
-    x_numeric_robust(:, k+1) = p.model.A * x_numeric_robust(:, k) + p.model.B * u_numeric_robust(k) + w;
+    x_numeric_exact(:, k+1) = p.model.A * x_numeric_exact(:, k) + p.model.B * u_numeric_exact(k) + w(:, k);
+    x_numeric_robust(:, k+1) = p.model.A * x_numeric_robust(:, k) + p.model.B * u_numeric_robust(k) + w(:, k);
     
     % Compute regret (Open Loop)
-    regret_numeric(k) = fval_numeric_robust - fval_numeric_exact;
+    regret_open_loop_numeric(k) = fval_numeric_robust - fval_numeric_exact;
     
     % Compute closed loop regret
     state_contribution = MatrixWeightedNorm(x_numeric_robust, Q) - MatrixWeightedNorm(x_numeric_exact, Q);
     control_contribution = MatrixWeightedNorm(u_numeric_robust, R) - MatrixWeightedNorm(u_numeric_exact, R);
-    regret_closed_loop(k) = state_contribution + control_contribution;
+    regret_closed_loop_numeric(k) = state_contribution + control_contribution;
     
     % compute infinite horizon total regret
-    regret_total(k) = regret_numeric(k) + regret_closed_loop(k);
+    regret_total_numeric(k) = regret_open_loop_numeric(k) + regret_closed_loop_numeric(k);
     
     % =================================================================== %
     % Analytic solution of QP
@@ -129,6 +145,41 @@ for k = 1 : 1 : length(t)-1
         % Lagrangian system
         u_analytic_exact(k) = u_seq_analytic_exact(1);
         
+        P_analytic_exact = (p.H_inv * M_tilde' / (M_tilde * p.H_inv * M_tilde' + 1E-08 * eye(size(M_tilde, 1))) * M_tilde * p.H_inv - p.H_inv);
+        q_analytic_exact = p.H_inv * M_tilde' / (M_tilde * p.H_inv * M_tilde' + 1E-08 * eye(size(M_tilde, 1))) * b_tilde;
+        P_closed_loop_analytic_exact(k, :) = P_analytic_exact(1,:);
+        q_closed_loop_analytic_exact(1, k) = q_analytic_exact(1);
+        
+        % running_sum_exact = SUM (A^i(B*gamma + w))
+        if(k > 1)            
+            for kk = 1:k-1
+                running_sum_exact(:, k) = running_sum_exact(:, k) + (p.model.A)^(kk)*(p.model.B*gamma_closed_loop_exact(1, kk) + ...
+                    w(:, kk));
+            end
+            gamma_closed_loop_exact(1, k) = q_closed_loop_analytic_exact(1, k) + ...
+                P_closed_loop_analytic_exact(k, :)*running_sum_exact(:, k);
+        end
+        
+        % Update the u_closed_loop_analytic_exact
+        if(k == 1)
+            psi_exact(:, k) = P_closed_loop_analytic_exact(k, :);
+        else
+            % runningStateSum = Sum(A^i*B*\Psi)
+            runningStateSum = zeros(p.nx, 1);
+            if(k > 1)            
+                for kk = 1:k-1
+                    runningStateSum = runningStateSum + (p.model.A)^(kk)*(p.model.B*psi_exact(:, kk));
+                end            
+            end
+            psi_exact(:, k) = P_closed_loop_analytic_exact(k, :)*((p.model.A)^(k) + runningStateSum);
+        end        
+        u_closed_loop_analytic_exact(:, k) = psi_exact(:, k)*x_closed_loop_analytic_exact(:, 1) + gamma_closed_loop_exact(1, k);
+              
+        
+        % Update the x_closed_loop_analytic_exact
+        x_closed_loop_analytic_exact(:, k+1) = ((p.model.A)^(k) + runningStateSum)*x_closed_loop_analytic_exact(:, 1) + running_sum_exact;        
+        
+        
     end
     
     %%%% Distributionally robust tightening of state constraints
@@ -155,6 +206,40 @@ for k = 1 : 1 : length(t)-1
         % Lagrangian system
         u_analytic_robust(k) = u_seq_analytic_robust(1);
         
+        P_analytic_robust = (p.H_inv * M_tilde' / (M_tilde * p.H_inv * M_tilde' + 1E-08 * eye(size(M_tilde, 1))) * M_tilde * p.H_inv - p.H_inv);
+        q_analytic_robust = p.H_inv * M_tilde' / (M_tilde * p.H_inv * M_tilde' + 1E-08 * eye(size(M_tilde, 1))) * b_tilde;
+        P_closed_loop_analytic_robust(k, :) = P_analytic_robust(1,:);
+        q_closed_loop_analytic_robust(1, k) = q_analytic_robust(1);
+        
+        if(k > 1)            
+            for kk = 1:k-1
+                running_sum_robust(:, k) = running_sum_robust(:, k) + (p.model.A)^(kk)*(p.model.B*gamma_closed_loop_robust(1, kk) + ...
+                    w(:, kk));
+            end
+            gamma_closed_loop_robust(1, k) = q_closed_loop_analytic_robust(1, k) + ...
+                P_closed_loop_analytic_robust(k, :)*running_sum_robust(:, k);
+        end
+        
+        % Update the u_closed_loop_analytic_robust
+        if(k == 1)
+            psi_robust(:, k) = P_closed_loop_analytic_robust(k, :);
+        else
+            % runningStateSum = Sum(A^i*B*\Psi)
+            runningStateSum = zeros(p.nx, 1);
+            if(k > 1)            
+                for kk = 1:k-1
+                    runningStateSum = runningStateSum + (p.model.A)^(kk)*(p.model.B*psi_robust(:, kk));
+                end            
+            end
+            psi_robust(:, k) = P_closed_loop_analytic_robust(k, :)*((p.model.A)^(k) + runningStateSum);
+        end        
+        u_closed_loop_analytic_robust(:, k) = psi_robust(:, k)*x_closed_loop_analytic_robust(:, 1) + gamma_closed_loop_robust(1, k);
+              
+        
+        % Update the x_closed_loop_analytic_robust
+        x_closed_loop_analytic_robust(:, k+1) = ((p.model.A)^(k) + runningStateSum)*x_closed_loop_analytic_robust(:, 1) + running_sum_robust;        
+        
+        
     end
     
     % Record the time when both systems enter the set Phi
@@ -171,13 +256,24 @@ for k = 1 : 1 : length(t)-1
         p.fT(x_numeric_robust(:, k)) * u_seq_analytic_robust + ...
         p.const(x_numeric_robust(:, k));
     
-    regret_analytic(k) = fval_analytic_robust - fval_analytic_exact;
+    regret_open_loop_analytic(k) = fval_analytic_robust - fval_analytic_exact;
     
 end
 
-% Compute the accumuated numeric and analytic regrets
-cumulative_regret_numeric = cumsum(regret_numeric);
-cumulative_regret_analytic = cumsum(regret_analytic);
+% Compute analytic closed loop regret
+for k = 2:length(t)-1
+    xclExactSq = MatrixWeightedNorm(x_closed_loop_analytic_exact(:, k), Q);
+    xclRobustSq = MatrixWeightedNorm(x_closed_loop_analytic_robust(:, k), Q);
+    uclExactSq = MatrixWeightedNorm(u_closed_loop_analytic_exact(:, k), R);
+    uclRobustSq = MatrixWeightedNorm(u_closed_loop_analytic_robust(:, k), R);
+    regret_closed_loop_analytic(1, k) = regret_closed_loop_analytic(1, k-1) + (xclRobustSq + uclRobustSq) - (xclExactSq + uclExactSq);
+end
+
+% Compute analytic total regret
+for k = 1:length(t)-1
+    regret_total_analytic(1, k) = regret_open_loop_analytic(k) + regret_closed_loop_analytic(1, k);
+end
+
 
 % Display the time when both systems entered the set Phi
 fprintf('Time step when both systems enter Phi: %d \n', phiTimeEnter);
@@ -235,7 +331,7 @@ gca.YAxis.TickLabelFormat = '\\textbf{%g}';
 matlab2tikz('figurehandle',figure2,'filename','x2.tex' ,'standalone', true, 'showInfo', false);
 
 
-figure4 = figure('Color',[1 1 1]);
+figure3 = figure('Color',[1 1 1]);
 hold on; grid on;
 % First state
 plot(t, x_numeric_exact(1, :), '-.', 'Linewidth', 5);
@@ -265,22 +361,22 @@ set(a, 'FontSize', 70);
 gca.XAxis.TickLabelFormat = '\\textbf{%g}';
 gca.YAxis.TickLabelFormat = '\\textbf{%g}';
 % Convert matlab figs to tikz for pgfplots in latex document.
-matlab2tikz('figurehandle',figure4,'filename','states.tex' ,'standalone', true, 'showInfo', false);
+matlab2tikz('figurehandle',figure3,'filename','states.tex' ,'standalone', true, 'showInfo', false);
 
 % Total Regret (Numerically Computed)
-figure3 = figure('Color',[1 1 1]);
+figure4 = figure('Color',[1 1 1]);
 hold on; grid on;
-plot(t(1:end-1), regret_numeric, 'Linewidth', 2);
-plot(t(1:end-1), regret_closed_loop, 'Linewidth', 2);
-plot(t(1:end-1), regret_total, 'Linewidth', 2);
+plot(t(1:end-1), regret_open_loop_numeric, 'Linewidth', 2);
+plot(t(1:end-1), regret_closed_loop_numeric, 'Linewidth', 2);
+plot(t(1:end-1), regret_total_numeric, 'Linewidth', 2);
 % Plot the start of time entering into set Phi
 phiEntryTime = 4.1;
-yVector = min(regret_closed_loop) - 10:max(regret_total);
+yVector = min(regret_closed_loop_numeric) - 10:max(regret_total_numeric);
 plot(phiEntryTime*ones(size(yVector)), yVector, '--', 'Linewidth', 5);
 xlabel('t (s)'); 
 legend('$R^{\mathrm{ol}}$', '$R^{\mathrm{cl}}$', '$R^{\mathrm{total}}$');
 xlim([t(1), t(end)]);
-ylim([min(regret_closed_loop) - 10, max(regret_total) + 20]);
+ylim([min(regret_closed_loop_numeric) - 10, max(regret_total_numeric) + 20]);
 text(4.3,20,'$\tau_{\Phi}$', 'FontSize', 50);
 a = findobj(gcf, 'type', 'axes');
 h = findobj(gcf, 'type', 'line');
@@ -290,7 +386,32 @@ set(a, 'FontSize', 50);
 gca.XAxis.TickLabelFormat = '\\textbf{%g}';
 gca.YAxis.TickLabelFormat = '\\textbf{%g}';
 % Convert matlab figs to tikz for pgfplots in latex document.
-matlab2tikz('figurehandle',figure3,'filename','regrets.tex' ,'standalone', true, 'showInfo', false);
+matlab2tikz('figurehandle',figure4,'filename','regrets.tex' ,'standalone', true, 'showInfo', false);
+
+% Total Regret (Analytically Computed)
+figure5 = figure('Color',[1 1 1]);
+hold on; grid on;
+plot(t(1:end-1), regret_open_loop_analytic, 'Linewidth', 2);
+plot(t(1:end-1), regret_closed_loop_analytic, 'Linewidth', 2);
+plot(t(1:end-1), regret_total_analytic, 'Linewidth', 2);
+% Plot the start of time entering into set Phi
+phiEntryTime = 4.1;
+yVector = min(regret_closed_loop_numeric) - 10:max(regret_total_numeric);
+plot(phiEntryTime*ones(size(yVector)), yVector, '--', 'Linewidth', 5);
+xlabel('t (s)'); 
+legend('$R^{\mathrm{ol}}$', '$R^{\mathrm{cl}}$', '$R^{\mathrm{total}}$');
+xlim([t(1), t(end)]);
+ylim([min(regret_closed_loop_numeric) - 10, max(regret_total_numeric) + 20]);
+text(4.3,20,'$\tau_{\Phi}$', 'FontSize', 50);
+a = findobj(gcf, 'type', 'axes');
+h = findobj(gcf, 'type', 'line');
+set(h, 'linewidth', 10);
+set(a, 'linewidth', 10);
+set(a, 'FontSize', 50);
+gca.XAxis.TickLabelFormat = '\\textbf{%g}';
+gca.YAxis.TickLabelFormat = '\\textbf{%g}';
+% Convert matlab figs to tikz for pgfplots in latex document.
+matlab2tikz('figurehandle',figure5,'filename','analyticregrets.tex' ,'standalone', true, 'showInfo', false);
 
 
 function [normResult] = MatrixWeightedNorm(zeta, Z)
